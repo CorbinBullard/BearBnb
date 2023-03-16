fa4090619b84c593b6d400c3bb21f04feed8a7d1
Corbin <corbindbullard@gmail.com>
2023-03-16 15:44:26 -0400

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,39 @@
 const router = require('express').Router();
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js')
+const { User } = require('../../db/models');
 
 
 // router.post('/test', function (req, res) {
 //     res.json({ requestBody: req.body });
 // });
 
+// GET /api/set-token-cookie
 
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+            username: 'DemoUser'
+        }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
+});
 
+router.use(restoreUser);
 
+router.get(
+    '/restore-user',
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
 
-
-
-
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+        return res.json(req.user);
+    }
+);
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
+router.use('/api', apiRouter);
 
 // router.get('/hello/world', function (req, res) {
 //     res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -16,6 +17,7 @@ router.get("/api/csrf/restore", (req, res) => {
     });
 });
 
-router.use('/api', apiRouter);
+
+
 
 module.exports = router;
