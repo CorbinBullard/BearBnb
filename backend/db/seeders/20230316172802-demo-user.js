c54ef7180a653750837aa5493d3262acb40f0c1f
Corbin <corbindbullard@gmail.com>
2023-03-24 12:35:51 -0400

    Capitalized N in firstName

--- backend/db/seeders/20230316172802-demo-user.js
+++ backend/db/seeders/20230316172802-demo-user.js
@@ -30,28 +30,28 @@ module.exports = {
       email: 'demo@user.io',
       username: 'DemoUser',
       hashedPassword: bcrypt.hashSync('password'),
-      firstname: 'User',
+      firstName: 'User',
       lastName: 'One'
     },
      {
        email: 'user1@user.io',
        username: 'FakeUser1',
        hashedPassword: bcrypt.hashSync('password1'),
-       firstname: 'User',
+       firstName: 'User',
        lastName: 'Two'
      },
      {
        email: 'user2@user.io',
        username: 'FakeUser2',
        hashedPassword: bcrypt.hashSync('password2'),
-       firstname: 'User',
+       firstName: 'User',
        lastName: 'Three'
      },
      {
        email: 'user3@user.io',
        username: 'FakeUser3',
        hashedPassword: bcrypt.hashSync('password3'),
-       firstname: 'User',
+       firstName: 'User',
        lastName: 'Four'
      }
    ])
@@ -65,7 +65,7 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    
+
     await queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['DemoUser', 'FakeUser1', 'FakeUser2', 'FakeUser3'] }
     }, {});
