dd41b48824b8024187ece15a948e51152ec177db
Corbin <corbindbullard@gmail.com>
2023-04-07 17:54:36 -0400

    finished Modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignUpFormPage";
+import LoginFormModal from "./components/LoginFormModal";
+import SignupFormPage from "./components/SignupFormModal";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -18,12 +18,7 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path={"/login"}>
-            <LoginFormPage />
-          </Route>
-          <Route path={"/signup"} >
-            <SignupFormPage />
-          </Route>
+          
         </Switch>
       )}
     </>
--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -2,33 +2,35 @@ import { useState } from "react";
 import * as sessionActions from "../../store/session.js";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { useModal } from "../../context/Modal.js";
 import "./LoginForm.css";
 
-const LoginFormPage = () => {
+const LoginFormModal = () => {
     const dispatch = useDispatch();
     const sessionUser = useSelector((state) => state.session.user);
     const [credential, setCredential] = useState("");
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState({});
-
+    const { closeModal } = useModal();
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setErrors({});
-        return dispatch(sessionActions.login({ credential, password })).catch(
-            async (res) => {
+        return dispatch(sessionActions.login({ credential, password }))
+            .then(closeModal)
+            .catch(async (res) => {
                 const data = await res.json();
-                if (data && data.errors) setErrors(data.errors);
-            }
-        );
+                if (data && data.errors) {
+                    setErrors(data.errors);
+                }
+            });
     };
 
     return (
         <>
             <h1>Log In</h1>
-            <form onSubmit={handleSubmit}
-            className="login-form">
+            <form onSubmit={handleSubmit}>
                 <label>
                     Username or Email
                     <input
@@ -47,11 +49,13 @@ const LoginFormPage = () => {
                         required
                     />
                 </label>
-                {errors.credential && <p>{errors.credential}</p>}
+                {errors.credential && (
+                    <p>{errors.credential}</p>
+                )}
                 <button type="submit">Log In</button>
             </form>
         </>
     )
 }
 
-export default LoginFormPage;
+export default LoginFormModal;
